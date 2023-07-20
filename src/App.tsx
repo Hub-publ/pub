1fc85c8ad2c1c017edb564e916e8b6d204c743c7
hubdncajy <hubdncajy@gmail.com>
2023-07-20 19:54:52 +0900

    230720_merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./page/index";
+import IndexAjy from "./_ajy/page/index";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Main />}></Route>
+          <Route path=""></Route>
+          {/* 유진 */}
+          <Route path="/main" element={<Main />}></Route>
+          {/* 지연 */}
+          <Route path="/ajy" element={<IndexAjy />}></Route>
+          {/* 한주 */}
         </Routes>
       </BrowserRouter>
     </div>
