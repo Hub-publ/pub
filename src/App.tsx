85d1cc822efdcd0ef3bd0f1266b35632402e84d2
lhj <hubdnclhj3@gmail.com>
2023-07-20 19:23:10 +0900

    컴포넌트 작업진행

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./page/index";
+import LhjMain from "./_lhj/page/index";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          {/* 유진 */}
           <Route path="/" element={<Main />}></Route>
+          {/* 지연 */}
+          {/* 한주 */}
+          <Route path="/lhj" element={<LhjMain />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
