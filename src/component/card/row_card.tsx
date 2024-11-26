0b8153a243fd17e96af062fd44414706ba0cfcd1
hubdnchyj <hubdnchyj@gmail.com>
2024-11-26 14:57:10 +0900

    241126 List Save

--- src/component/card/row_card.tsx
+++ src/component/card/row_card.tsx
@@ -24,27 +24,26 @@ function RowCard(props: Props) {
           style={{ backgroundImage: `url(/img/${props.img_name})` }}
         ></div>
         <div className="cont_area">
-          {props.main_title ? (
-            <h1 className="main_title ellipsis">
-              <Tag
-                title={props.tag_title}
-                bg_color={props.tag_bg_color}
-                bd_color={props.tag_bd_color}
-                color={props.tag_color}
-                // bg_color="transparent"
-                // bd_color="tag-bd-red1"
-                // color="tag-text-red1"
-              />
-              <span className="ellipsis">{props.main_title}</span>
-            </h1>
-          ) : (
-            <></>
-          )}
-          {props.sub_title ? (
-            <p className="sub_title ellipsis">{props.sub_title}</p>
-          ) : (
-            <></>
-          )}
+          <div>
+            {props.main_title ? (
+              <h1 className="main_title ellipsis">
+                <Tag
+                  title={props.tag_title}
+                  bg_color={props.tag_bg_color}
+                  bd_color={props.tag_bd_color}
+                  color={props.tag_color}
+                />
+                <span className="ellipsis">{props.main_title}</span>
+              </h1>
+            ) : (
+              <></>
+            )}
+            {props.sub_title ? (
+              <p className="sub_title ellipsis">{props.sub_title}</p>
+            ) : (
+              <></>
+            )}
+          </div>
           {props.detail_option1 ||
           props.detail_option2 ||
           props.detail_option3 ? (
